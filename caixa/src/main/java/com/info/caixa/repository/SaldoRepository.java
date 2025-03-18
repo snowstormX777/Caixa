31435e93a4f19b662dfdbb413d931129436c22f4
snowstormX777 <richardsinfo2008@gmail.com>
2025-03-18 15:28:22 -0300

    Delete caixa directory

--- caixa/src/main/java/com/info/caixa/repository/SaldoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.info.caixa.repository;
-
-import com.info.caixa.model.Saldo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SaldoRepository extends JpaRepository<Saldo, Integer> {
-
-}
